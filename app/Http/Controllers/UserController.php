df3986cc8b4dde0ee034ad4a5757bb3015bb4c40
Briyan Adityatama <briyanwam@gmail.com>
2024-12-19 16:59:48 +0700

    add user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\GetUsersRequest;
+use App\Services\UserService;
+use Illuminate\Http\JsonResponse;
+
+class UserController extends Controller
+{
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function store(CreateUserRequest $request): JsonResponse
+    {
+        $user = $this->userService->createUser($request->validated());
+        return response()->json($user, 201);
+    }
+
+    public function index(GetUsersRequest $request): JsonResponse
+    {
+        $users = $this->userService->getUsers($request->validated());
+        return response()->json($users);
+    }
+}
